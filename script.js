42620e5416dcd073f79931ea7685000753ac83a0
GowharAhmadMir <ergowhar00@gmail.com>
2026-03-29 22:04:25 +0530

    Add search functionality for services

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function callNumber(number) {
   window.location.href = "tel:" + number ;
 }
+const searchInput = document.getElementById("search");
+
+searchInput.addEventListener("keyup", function () {
+  const value = searchInput.value.toLowerCase();
+  const services = document.querySelectorAll(".service");
+
+  services.forEach(service => {
+    const text = service.innerText.toLowerCase();
+    if (text.includes(value)) {
+      service.style.display = "block";
+    } else {
+      service.style.display = "none";
+    }
+  });
+});
